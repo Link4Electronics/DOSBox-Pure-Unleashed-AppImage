e2b9d5516499b8703b2ca88b6f18646648e83251
Link4Electronics <link4electronics@gmail.com>
2026-05-03 07:14:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,11 +16,13 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+echo "Maing nightly build of DOSBox Pure Unleashed..."
+echo "---------------------------------------------------------------"
+git clone https://github.com/schellingb/dosbox-pure-unleashed
+git clone https://github.com/schellingb/dosbox-pure
+git clone https://github.com/schellingb/ZillaLib
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cd dosbox-pure-unleashed
+make linux-release ZL_VIDEO_OPENGL_CORE=1 -j
+mv -v Release-linux/DOSBoxPure_x86_64 ./AppDir/bin
